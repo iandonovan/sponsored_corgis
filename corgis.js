8004d52238e64fa0808df262c98634121b221a53
Ian Donovan <ian.donovan@tufts.edu>
2013-06-25 22:55:11 -0400

    Light refactor, starting on the Swap Corgi functionality in #2

--- corgis.js
+++ corgis.js
@@ -10,7 +10,6 @@ function corgi_cover(){
 	for (var i=0; i<side_ads.length; i++){
 		corgi_attack(side_ads[i]);
 	}
-
 	// And in the actual newsfeed
 	var stories = document.getElementsByClassName("uiUnifiedStory");
 	for (var i=0; i<stories.length; i++){
@@ -20,7 +19,6 @@ function corgi_cover(){
 			corgi_attack(story);
 		}
 	}
-
 	// The big one you get when you log out
 	var logged_out_ads = document.getElementsByClassName("loggedOutAuxLinks");
 	for (var i=0; i<logged_out_ads.length; i++){
@@ -30,25 +28,34 @@ function corgi_cover(){
 }
 
 function corgi_attack(ad_story){
-	// Don't replace an ad over and over
-	if (!ad_story.hasAttribute("corgified")){
-		// Mimic ad properties
-		var ad_height = ad_story.clientHeight;
-		var ad_width = ad_story.clientWidth;
+	// Pick a random corgi picture and replace the ad with it
+	var corgi_img_URL = pick_corgi();
+	var corgi_story = place_corgi(ad_story, corgi_img_URL);
+	// Click a corgi to swap a new one in
+	corgi_story.onClick = swap_corgi;
+}
 
-		// Pick a random corgi picture
-		var corgi_URLs = JSON.parse(localStorage["corgi_URLs"]);
-		var image_index = Math.round(Math.random() * (corgi_URLs.length - 1));
-		var corgi_img_URL = corgi_URLs[image_index];
+function pick_corgi(){
+	var corgi_URLs = JSON.parse(localStorage["corgi_URLs"]);
+	var image_index = Math.round(Math.random() * (corgi_URLs.length - 1));
+	var corgi_img_URL = corgi_URLs[image_index];
+	return corgi_img_URL;
+}
 
-		// Create the corgi picture on the page
-		var corgi_img = document.createElement("img");
-		corgi_img.src = corgi_img_URL;
-		corgi_img.height = ad_height;
-		corgi_img.width = ad_width;
-		ad_story.parentNode.replaceChild(corgi_img, ad_story);
-		ad_story.setAttribute("corgified", "true");
-	}
+function place_corgi(ad_story, corgi_img_URL){
+	var ad_height = ad_story.clientHeight;
+	var ad_width = ad_story.clientWidth;
+	var corgi_img = document.createElement("img");
+	corgi_img.src = corgi_img_URL;
+	corgi_img.height = ad_height;
+	corgi_img.width = ad_width;
+	corgi_story = ad_story.parentNode.replaceChild(corgi_img, ad_story);
+	return corgi_story;
+}
+
+function swap_corgi(){
+	console.log("Called swap on " + this);
+	this.src = pick_corgi();
 }
 
 get_localstorage_URLs();
